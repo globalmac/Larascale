d45a87934f89f595586745720f08eb07808e12ec
Igor Pankov <admin@blind-cobra.com>
2016-05-04 16:46:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -139,7 +139,7 @@ apt-get update -y > /dev/null 2>&1
 echo mysql-server mysql-server/root_password select $MYSQL_ROOT_PASSWORD | debconf-set-selections
 echo mysql-server mysql-server/root_password_again select $MYSQL_ROOT_PASSWORD | debconf-set-selections
 
-apt-get install percona-server-server-5.5 -y --force-yes -qq > /dev/null 2>&1
+apt-get install -y percona-server-server-5.5 percona-server-client-5.5 --show-progress --allow-remove-essential --quiet
 
 mysql -e "CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'" -u root -p$MYSQL_ROOT_PASSWORD
 mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'" -u root -p$MYSQL_ROOT_PASSWORD
