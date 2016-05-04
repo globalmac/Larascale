23d1a421c713d04ce5201875636534d39ae2f96d
Igor Pankov <admin@blind-cobra.com>
2016-05-04 13:39:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -112,8 +112,8 @@ echo "=========== Install Percona XtraDB Server ==========="
 echo
 
 apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A > /dev/null 2>&1
-echo "deb http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list > /dev/null 2>&1
-echo "deb-src http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list > /dev/null 2>&1
+echo "deb http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list
+echo "deb-src http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list
 
 apt-get update -y --force-yes -qq > /dev/null 2>&1
 
