e752ba02f3d49ad4de9a7d9b05a53eee5e0f2660
Igor Pankov <admin@blind-cobra.com>
2017-01-13 22:21:11 +0300

    Issue #1 - добавил php7.0-xml

--- install-postgresql.sh
+++ install-postgresql.sh
@@ -99,7 +99,7 @@ echo
 echo "- Installing, please wait..."
 echo
 
-apt-get install php7.0-fpm php7.0-common php7.0-gd php7.0-pgsql php7.0-curl php7.0-cli php-pear php7.0-dev php7.0-imap php7.0-mcrypt php7.0-readline php7.0-mbstring php7.0-json php7.0-zip memcached php7.0-memcached php7.0-imagick imagemagick -y --force-yes -qq > /dev/null 2>&1
+apt-get install php7.0-fpm php7.0-common php7.0-gd php7.0-pgsql php7.0-curl php7.0-cli php-pear php7.0-dev php7.0-imap php7.0-mcrypt php7.0-readline php7.0-mbstring php7.0-json php7.0-zip memcached php7.0-memcached php7.0-imagick php7.0-xml imagemagick -y --force-yes -qq > /dev/null 2>&1
 
 echo
 echo "==> PHP7 installed succesful!"
